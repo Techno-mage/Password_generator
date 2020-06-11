e51b28bda6a6dc8b65d9976c707bf5bdbbfe0d71
Erik Schaal <schaa077@umn.edu>
2020-06-11 10:54:23 -0500

    added a random selector function

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+//Random selector function
+function randomSelector(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
 // Generate password
 function generatePassword(){
   
   //VARIABES
 
   //boolians for each type of character
-
-  
   do{
     var upper = confirm("Do you want to use Upper case?");
     var lower = confirm("Do you want to use Lower case?");
@@ -31,6 +34,19 @@ function generatePassword(){
     }
 
   } while(chars < 8 || chars > 128 || chars == NaN);
+
+  //Assemble the passworld
+  //variable for password generation
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var specchar = "!@#$%&*?.,-_+=";
+  var uLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+
+
+  //insure that passworld has at least one of each selected value.
+
+
+
 
 
 }
