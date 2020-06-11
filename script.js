6bf0f52f7f4f36a924cd94644a4aa20e724319fa
Erik Schaal <schaa077@umn.edu>
2020-06-10 19:29:43 -0500

    base variables added, with basic error checking

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+// Generate password
+function generatePassword(){
+  
+  //VARIABES
+
+  //boolians for each type of character
+
+  
+  do{
+    var upper = confirm("Do you want to use Upper case?");
+    var lower = confirm("Do you want to use Lower case?");
+    var nums = confirm("Do you want to use numbers?");
+    var spec = confirm("Do you want to use Special characters?");
+
+    if (!upper && !lower && !nums && !spec){
+      alert("please choose at least one option.");
+    }
+
+  } while(!upper && !lower && !nums && !spec)
+  
+  do{
+    var chars = prompt("please enter the number of characters you want, between 8 and 128.");
+    
+    chars = Number(chars);
+    
+    if (chars == NaN){
+      
+      alert("Please enter a valid responce.");
+    } else if (chars < 8 || chars > 128){
+      alert("password must be between 8 and 128 characters.");
+    }
+
+  } while(chars < 8 || chars > 128 || chars == NaN);
+
+
+}
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
